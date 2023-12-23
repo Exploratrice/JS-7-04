19bdddc0d2559b6228aef29d41c4fb0862feec1f
Explora <exploratrice2019@mail.ru>
2023-12-23 17:00:13 +0300

    second task

--- gh.test.js
+++ gh.test.js
@@ -3,25 +3,25 @@ let page;
 beforeEach(async () => {
   page = await browser.newPage();
   await page.goto("https://github.com/team");
-}, 60000);
+}, 300000);
 
 afterEach(() => {
   page.close();
 });
 
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
+  test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
-  }, 5000);
+  }, 6000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  }, 5000);
+  }, 6000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -30,5 +30,35 @@ describe("Github page tests", () => {
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Get started with Team")
-  }, 5000);
+  }, 6000);
+});
+
+describe("Titles of other pages", () => {
+  
+  test("The h1 header content", async () => {
+    await page.goto("https://github.com/marketplace");
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub Marketplace · to improve your workflow · GitHub');
+  }, 6000);
+
+  test("The h1 header content", async () => {
+    await page.goto("https://github.com/topics");
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Topics on GitHub · GitHub');
+  }, 6000);
+
+  test("The h1 header content", async () => {
+    await page.goto("https://github.com/events");
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub Events · GitHub');
+  }, 6000);
 });
